2da9febc57065974ab52daedbb7ff9914c84544a
arguskos <mark.furman@student.howest.be>
2017-03-28 17:29:13 +0400

    new

--- Assets/Scripts/Cequencer.cs
+++ Assets/Scripts/Cequencer.cs
@@ -10,10 +10,16 @@ public class Cequencer : MonoBehaviour
     private int _lettersOnObject = 6;
     private int _lettersInSequence = 3;
     public GameObject Cube;
+    public GameObject DebugSequence;
+    public Material CubeMaterial;
     public List<int> LettersOnBox;
-    public List<int> LettersInSequince;
-    private  int _currentLetter=0;
+    public List<KeyValuePair<int, int>> LettersInSequence = new List<KeyValuePair<int, int>>();
+    private int _currentLetter = 0;
     // Use this for initialization
+    private float _emissive;
+    private bool _isShowing = true;
+    private Material _currentMat;
+    private float _speed = 20;
     void Start()
     {
         for (int i = 0; i < _lettersOnObject; i++)
@@ -21,21 +27,56 @@ public class Cequencer : MonoBehaviour
             int rnd = Random.Range(0, Alphabet);
             while (LettersOnBox.Contains(rnd))
             {
-                rnd= Random.Range(0, Alphabet);
+                rnd = Random.Range(0, Alphabet);
             }
             LettersOnBox.Add(rnd);
         }
         for (int i = 0; i < _lettersInSequence; i++)
         {
-            LettersInSequince.Add(LettersOnBox[Random.Range(0,LettersOnBox.Count)]);
+            int r = LettersOnBox[Random.Range(0, LettersOnBox.Count)];
+            LettersInSequence.Add(new KeyValuePair<int, int>(r, LettersOnBox.IndexOf(r)));
         }
 
 
-        for (int i = 0; i < LettersInSequince.Count; i++)
+        for (int i = 0; i < LettersOnBox.Count; i++)
         {
-            Debug.Log(LettersInSequince[i]);
+            Debug.Log(LettersOnBox[i]);
         }
-        //StartCoroutine("Show", LettersArray[_currentLetter]);
+        print("");
+        for (int i = 0; i < LettersInSequence.Count; i++)
+        {
+            Debug.Log(LettersInSequence[i]);
+        }
+        int counter = 0;
+        foreach (Transform child in Cube.transform)
+        {
+
+            child.GetComponent<Renderer>().material = CubeMaterial;
+            int l = LettersOnBox[counter];
+            print("x " + Mathf.Floor(l / 4) + "   y: " + (l % 4));
+
+            child.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(Mathf.Floor(l / 4) * 0.25f, (l % 4) * 0.25f));
+                counter++;
+            }
+
+            counter = 0;
+        foreach (Transform child in DebugSequence.transform)
+        {
+
+            child.GetComponent<Renderer>().material = CubeMaterial;
+            int l = LettersInSequence[counter].Key;
+            print("x " + Mathf.Floor(l / 4) + "   y: " + (l % 4));
+
+            child.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(Mathf.Floor(l / 4) * 0.25f, (l % 4) * 0.25f));
+            counter++;
+        }
+        Debug.Log(LettersInSequence[_currentLetter].Value);
+
+
+        Renderer renderer = Cube.transform.GetChild(LettersInSequence[_currentLetter].Value).GetComponent<Renderer>();
+        _currentMat = renderer.material;
+
+        //StartCoroutine("Show", LettersInSequence[_currentLetter].Value);
     }
 
     // Update is called once per frame
@@ -54,13 +95,52 @@ public class Cequencer : MonoBehaviour
 
         //    mat.SetColor("_EmissionColor", finalColor);
         //}
-    }
-    IEnumerator Show(int id )
-    {
-        for (float f = 0f; f <= 0.3; f += 0.009f)
+
+     
+        float emission = _emissive;
+        Color baseColor = Color.red;
+        //Replace this with whatever you want for your base color at emission level '1'
+
+        Color finalColor = baseColor * Mathf.LinearToGammaSpace(emission);
+
+        _currentMat.SetColor("_EmissionColor", finalColor);
+        if (_isShowing)
         {
-            Renderer renderer = Cube.GetComponent<Renderer>();
-            Material mat = renderer.materials[id];
+            _emissive += Time.deltaTime;
+            if (_emissive >= 1)
+            {
+                _isShowing = false;
+            }
+
+        }
+        else
+        {
+            _emissive -= Time.deltaTime;
+            
+            if (_emissive <= 0)
+            {
+                print("CurrLetter"+_currentLetter);
+                _isShowing = true;
+                Renderer renderer = Cube.transform.GetChild(LettersInSequence[_currentLetter].Value).GetComponent<Renderer>();
+                _currentMat = renderer.material;
+
+                _currentLetter += 1;
+                if (_currentLetter >= LettersInSequence.Count)
+                {
+                    _currentLetter = 0;
+
+                }
+            }
+        }
+    }
+
+    IEnumerator Show(int id)
+    {
+        Debug.Log("show started:" + id);
+        for (float f = 0f; f <= 0.7; f += 0.009f)
+        {
+            Renderer renderer = Cube.transform.GetChild(id).GetComponent<Renderer>();
+            Material mat = renderer.material;
 
             float emission = f;
             Color baseColor = Color.red;
@@ -71,17 +151,20 @@ public class Cequencer : MonoBehaviour
             mat.SetColor("_EmissionColor", finalColor);
             yield return null;
         }
-        StartCoroutine("Hide",id);
+        Debug.Log("show ended:" + id);
+
+        StartCoroutine("Hide", id);
 
     }
 
     IEnumerator Hide(int id)
     {
-        for (float f = 0.3f; f >=0; f -= 0.009f)
-        {
-            Renderer renderer = Cube.GetComponent<Renderer>();
+        Debug.Log("hide started:" + id);
 
-            Material mat = renderer.materials[id];
+        for (float f = 0.7f; f >= 0; f -= 0.009f)
+        {
+            Renderer renderer = Cube.transform.GetChild(id).GetComponent<Renderer>();
+            Material mat = renderer.material;
 
             float emission = f;
             Color baseColor = Color.red;
@@ -92,13 +175,15 @@ public class Cequencer : MonoBehaviour
             mat.SetColor("_EmissionColor", finalColor);
             yield return null;
         }
-        yield return new WaitForSeconds(1);
+        //yield return new WaitForSeconds(1);
         _currentLetter += 1;
-        if (_currentLetter >= LettersInSequince.Count)
+        if (_currentLetter >= LettersInSequence.Count)
         {
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(0);
             _currentLetter = 0;
         }
-        StartCoroutine("Show", LettersInSequince[_currentLetter ]);
+        Debug.Log("hide ended:" + id);
+
+        StartCoroutine("Show", LettersInSequence[_currentLetter].Value);
     }
 }
---
+++ Assets/Scripts/InputBase.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputBase : MonoBehaviour
+{
+
+    // Use this for initialization
+    public GameObject UserSequence;
+    public Cequencer Cequencer;
+    private List<int> _sequence = new List<int>();
+    public Material Mat;
+    private int _currentLetter = 0;
+    void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            _sequence.Add(0);
+        }
+    }
+
+    // Update is called once per frame
+    private void OnButtonPress(int x, int y)
+    {
+
+
+        var obj = UserSequence.transform.GetChild(_currentLetter);
+        _sequence[_currentLetter] = x * 4 + y;
+        
+        obj.GetComponent<Renderer>().material = Mat;
+
+        obj.GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(x * 0.25f, y * 0.25f));
+    }
+
+    private void Check()
+    {
+        bool right = true;
+        for (int i = 0; i < Cequencer.LettersInSequence.Count; i++)
+        {
+
+            if (Cequencer.LettersInSequence[i].Key != _sequence[i])
+            {
+                right = false;
+                break;
+                
+            }
+            
+        }
+        if (right)
+            print("RIGHT");
+        else
+        {
+            print("WRONG");
+        }
+     
+    }
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Check();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _currentLetter = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _currentLetter = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _currentLetter = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            OnButtonPress(0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+
+        {
+            OnButtonPress(0, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            OnButtonPress(0, 2);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            OnButtonPress(0, 3);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            OnButtonPress(1, 0);
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            OnButtonPress(1, 1);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            OnButtonPress(1, 2);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            OnButtonPress(1, 3);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            OnButtonPress(2, 0);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            OnButtonPress(2, 1);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            OnButtonPress(2, 2);
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            OnButtonPress(2, 3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            OnButtonPress(3, 0);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            OnButtonPress(3, 1);
+
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            OnButtonPress(3, 2);
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            OnButtonPress(3, 3);
+        }
+    }
+}
