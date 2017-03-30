90d4de9a4526f0e63087f7ccf1870570f6aa21cd
WouterWeynants <wouter.weynants@hotmail.com>
2017-03-30 15:17:59 +0200

    Changed art levels and added TestBox

---
+++ Assets/Scripts/UIEffectsTesting.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEffectsTesting : MonoBehaviour {
+
+    public float _defaultDelay = 0.25f;
+    public bool _isActive = false;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        //Activate
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _isActive = true;
+        }
+
+        //Deactivate
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            _isActive = false;
+        }
+
+        if (_isActive)
+        {
+            iTween.ScaleTo(gameObject, new Vector3(1.1f, 1.1f, 1.1f), _defaultDelay);
+        }
+        else
+        {
+            iTween.ScaleTo(gameObject, new Vector3(1.0f, 1.0f, 1.0f), _defaultDelay);
+        }
+    }
+}
