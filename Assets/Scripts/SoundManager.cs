8908dd50dd29043d68900659b29ee7067028c0ce
WouterWeynants <wouter.weynants@hotmail.com>
2017-03-31 09:52:34 +0200

    Added font family, soundmanager, sounds and updated sign effects

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+
+    //Initialize
+    public AudioSource[] soundArray = new AudioSource[4];
+
+    public enum Sound { Select, Wrong, Progress1, Progress2 };
+
+    //Play sound function
+    public void PlaySound(string soundName)
+    {
+        for (int i = 0; i < soundArray.Length; i++)
+        {
+            if (soundArray[i].name == soundName)
+            {
+                soundArray[i].Play();
+            }
+        }
+    }
+
+
+    public void PlaySound(Sound soundName)
+    {
+
+        soundArray[(int)soundName].Play();
+
+    }
+
+
+    //Loop sound function
+    public void ToggleLoop(string loopName)
+    {
+        for (int i = 0; i < soundArray.Length; i++)
+        {
+            if ((soundArray[i].name == loopName) && (!soundArray[i].isPlaying))
+            {
+                soundArray[i].Play();
+            }
+
+            else if (soundArray[i].name == loopName)
+            {
+                soundArray[i].Stop();
+            }
+        }
+    }
+
+
+    public void ToggleLoop(Sound soundName)
+    {
+
+        if ((!soundArray[(int)soundName].isPlaying))
+        {
+            soundArray[(int)soundName].Play();
+        }
+
+        else
+        {
+            soundArray[(int)soundName].Stop();
+        }
+
+    }
+
+
+
+    //Example of use
+    /*void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            PlaySound("Error");
+            ToggleLoop("MachineLoop");
+        }
+    }*/
+}
--- Assets/Scripts/UIEffectsTesting.cs
+++ Assets/Scripts/UIEffectsTesting.cs
@@ -6,37 +6,84 @@ public class UIEffectsTesting : MonoBehaviour {
 
     public float _defaultDelay = 0.25f;
     public bool _isActive = false;
+    public int _importanceLevel = 1;
 
+    private float _symbolScale = 1.0f;
+
+    public SoundManager _soundManager;
 
 	// Use this for initialization
 	void Start ()
     {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-
         //Activate
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.Y))
         {
             _isActive = true;
+            _soundManager.GetComponent<SoundManager>().PlaySound("Select");
         }
 
         //Deactivate
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.H))
         {
             _isActive = false;
+            _soundManager.GetComponent<SoundManager>().PlaySound("Select");
         }
 
+        //Level Up
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            if (_importanceLevel < 3)
+            {
+                _importanceLevel++;
+            }
+        }
+
+        //Level Down
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            if (_importanceLevel > 1)
+            {
+                _importanceLevel--;
+            }
+        }
+
+        //Set scale according to importanceLevel
+        if (_importanceLevel == 3)
+        {
+            _symbolScale = 1.0f;
+
+        }
+        else if (_importanceLevel == 2)
+        {
+            _symbolScale = 0.75f;
+        }
+        else if (_importanceLevel == 1)
+        {
+            _symbolScale = 0.35f;
+        }
+        else if (_importanceLevel == 0)
+        {
+            _symbolScale = 0.0f;
+        }
+
+        //Set scale according to isActive
         if (_isActive)
         {
-            iTween.ScaleTo(gameObject, new Vector3(1.1f, 1.1f, 1.1f), _defaultDelay);
+            _symbolScale *= 1.1f;
         }
         else
         {
-            iTween.ScaleTo(gameObject, new Vector3(1.0f, 1.0f, 1.0f), _defaultDelay);
+            _symbolScale *= 1.0f;
         }
+
+        //Scale to needed level on update
+        iTween.ScaleTo(gameObject, new Vector3(_symbolScale, _symbolScale, _symbolScale), _defaultDelay);
+
     }
 }
