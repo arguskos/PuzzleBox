1ecc1ffc8c92fea94959243741e7492e99a96b28
WouterWeynants <wouter.weynants@hotmail.com>
2017-03-31 18:03:56 +0200

    HexagonScore finished

--- Assets/Scripts/HexagonScoreViz.cs
+++ Assets/Scripts/HexagonScoreViz.cs
@@ -7,30 +7,51 @@ public class HexagonScoreViz : MonoBehaviour {
     public GameObject[] _Bones = new GameObject[6];
     public float[] _statValues = new float[6];
     public float _delay = 0.25f;
-    //private _localStartPos[];
+    private Vector3[] _boneStartPositions = new Vector3[6];
+    private bool[] _isActivated = new bool[6];
+    private float timeActive = 0.0f;
+
 
 	// Use this for initialization
 	void Start ()
     {
-        for (int i = 0; i < _statValues.Length; i++)
+        for (int i = 0; i < 6; i++)
         {
-            _statValues[i] = 1.0f;
+            _statValues[i] = 0.0f;
+            _boneStartPositions[i] = _Bones[i].transform.position;
+            _isActivated[i] = false;
         }
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        timeActive += Time.deltaTime;
+
         for (int i = 0; i < _statValues.Length; i++)
         {
             var amount = _statValues[i];
-            var newpos = new Vector3(0, 0, 0);
+            var newpos = this.gameObject.transform.position + ((_boneStartPositions[i] - this.gameObject.transform.position) * amount);
             iTween.MoveTo(_Bones[i], newpos, _delay);
+
+            //Debug
+            if (timeActive >= 0.5f+(((float)i)/6.0f) && _isActivated[i] == false)
+            {
+                _statValues[i] = Random.Range(0.1f, 1.0f);
+                _isActivated[i] = true;
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                _isActivated[i] = false;
+                _statValues[i] = 0.0f;
+            }
         }
     }
 
     //Set stat function
-    void SetStat(int pos, float amount)
+    public void SetStat(int pos, float amount)
     {
         _statValues[pos] = amount;
     }
